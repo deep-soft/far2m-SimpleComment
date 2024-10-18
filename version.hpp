92a6b3a092434e10e0dc794f4f15555105163e1b
deep-soft <deep-soft@users.noreply.github.com>
2024-10-18 07:59:34 +0300

    initial commit

---
+++ version.hpp
@@ -0,0 +1,7 @@
+#define PLUGIN_MAJOR    1
+#define PLUGIN_MINOR    0
+#define PLUGIN_DESC     L"Comment selected block"
+#define PLUGIN_NAME     L"SimpleComment"
+#define PLUGIN_FILENAME L"SimpleComment.dll"
+#define PLUGIN_AUTHOR   L"deep-soft ft Vladimir Panteleev"
+#define PLUGIN_VERSION  MAKEFARVERSION(PLUGIN_MAJOR, PLUGIN_MINOR, 0, 0, VS_RELEASE)
